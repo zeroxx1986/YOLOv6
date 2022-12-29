b03924c571a3b9bff3dfda8f863223ed190637ee
Tibor Bacsi <zeroxx1986@gmail.com>
2022-12-29 23:50:35 +0100

    hopefully final form

--- tools/infer_service.py
+++ tools/infer_service.py
@@ -5,6 +5,7 @@
 # from infer import run
 import os
 import sys
+import uuid
 
 ROOT = os.getcwd()
 if str(ROOT) not in sys.path:
@@ -22,15 +23,19 @@ app = Flask(__name__)
 @app.route('/inference', methods = ['POST'])
 def upload_file():
     data = request.get_data()
-    with open("image.jpg", "wb") as f:
+    name = uuid.uuid4()
+    with open(f"{name}.jpg", "wb") as f:
         f.write(data)
     # img_array = np.asarray(data, dtype=np.uint8)
     # imgdecoded = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    inferer = Inferer('/home/YOLOv6/image.jpg', '/home/YOLOv6/yolov6n_base.pt', '0', 'data/coco.yaml', [640,480], False)
+    inferer = Inferer(f'/home/YOLOv6/{name}.jpg', '/home/YOLOv6/yolov6n_base.pt', '0', 'data/coco.yaml', [640,480], False)
     inferer.infer(0.4, 0.45, [0], False, 1000, '/home/YOLOv6/runs/inference/exp', True, True, False, False, False)
 
-    with open("/home/YOLOv6/runs/inference/exp/image.txt", "r") as f:
+    with open(f'/home/YOLOv6/runs/inference/exp/{name}.txt', 'r') as f:
         ret = f.readlines()
+
+    os.remove(f'/home/YOLOv6/{name}.jpg')
+    os.remove(f'/home/YOLOv6/runs/inference/exp/{name}.txt')
 
     return jsonify(ret)
 
